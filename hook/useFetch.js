5b05d7ddbb5f05bf28b37aa60662980860acb12d
Denis W. Bonaccini <114507650+bonacciniWd@users.noreply.github.com>
2023-03-25 23:20:01 -0300

    Update useFetch.js

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -10,7 +10,7 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": 'dc3111ec46msh067623c04ed1abdp17aa8cjsnaf541ead2796',
+      "X-RapidAPI-Key": '',
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
